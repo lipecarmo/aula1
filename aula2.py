56ceda68f6752e88641a150203ddf404fe04567f
lipecarmo <lipeoliveira523@gmail.com>
2024-10-14 20:45:34 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,93 +1,93 @@
-# nome=input('Digite seu nome:')
+nome=input('Digite seu nome:')
 
-# idade=int(input('Digite sua idade:'))
+idade=int(input('Digite sua idade:'))
 
-# cidade=  input('Digite sua cidade:')
+cidade=  input('Digite sua cidade:')
 
-# print(f'Olá,{nome} Você tem {idade} anos e é de {cidade}.')
+print(f'Olá,{nome} Você tem {idade} anos e é de {cidade}.')
 
 # Atividade 01:
 # Comparação de idades:
 # Peça ao usuario duas idades e use operadores de comparação para verificar se a primeira idade é maior,menor ou igual á segunda.
 
-# idade1=int(input('Digite a primeira idade:'))
+idade1=int(input('Digite a primeira idade:'))
 
-# idade2=int(input('Digite a segunda idade:'))
+idade2=int(input('Digite a segunda idade:'))
 
 # Atividade 02:
 # Verificar igualdade de strings:
 # Peça ao usuário duas palavras e use operadores de comparação para verificar se elas são iguais
 
-# print(f'A primeira é maior que a segunda:{idade1>idade2}')
-# print(f'A primeira é menor que a segunda:{idade1<idade2}')
-# print(f'A primeira é igual a segunda{idade1==idade2}')
+print(f'A primeira é maior que a segunda:{idade1>idade2}')
+print(f'A primeira é menor que a segunda:{idade1<idade2}')
+print(f'A primeira é igual a segunda{idade1==idade2}')
 
-# idade=int(input('Digite sua idade:'))
-# habilitacao=input('voce tem habilitacao?')
-# print(f'Voce tem habilitacao? {idade>=18 and habilitacao=='sim'}')
+idade=int(input('Digite sua idade:'))
+habilitacao=input('voce tem habilitacao?')
+print(f'Voce tem habilitacao? {idade>=18 and habilitacao=='sim'}')
 
-# Atividade 04:Escreva um programa que peca duas notas de um aluno.use operadores logicos para verificar se as duas notas sao maiores ou iguais a 6
+Atividade 04:Escreva um programa que peca duas notas de um aluno.use operadores logicos para verificar se as duas notas sao maiores ou iguais a 6
 
-# nota1= float(input('Digite a primera nota:'))
+nota1= float(input('Digite a primera nota:'))
 
-# nota2= float(input('Digite a segunda nota:'))
+nota2= float(input('Digite a segunda nota:'))
 
-# print(nota1>= 6 and nota2>=6)
+print(nota1>= 6 and nota2>=6)
 
 # Atividade 09:Verificar par ou impar:
 # Crie um programa que peca ao usuario um numero e use a estrutura condicional if para verificar se ele e impar ou par.
 # numero = int(input('Digite um numero:'))
 
-# if numero % 2 == 0:
-#     print('Numero par')
-# else:
-#     print('numero impar')
+if numero % 2 == 0:
+    print('Numero par')
+else:
+    print('numero impar')
 
 # Atividade10:Verificar nota para aprovado e reprovado:Crie um programa que peca a nota de um aluno e use if para verificar se ele foi aprovado nota>=6.
 
-# nota= float(input('Digite sua nota:'))
+nota= float(input('Digite sua nota:'))
 
-# if nota >=6:
-#     print('Aprovado')
-# else:
-#     print('Reprovado')
+if nota >=6:
+    print('Aprovado')
+else:
+    print('Reprovado')
 
 # Atividade 09:
 # Verificar Par ou Ímpar e Positivo ou Negativo:
 # Escreva um programa que peça um número e use if para verificar
 # se ele é par ou ímpar e também se é positivo ou negativo.
 
-# numero= int(input('Digite um numero:'))
+numero= int(input('Digite um numero:'))
 
-# if numero % 2 == 0 and numero>0:
-#     print('É par e positivo')
-# elif numero % 2 == 0 and numero < 0:
-#     print('É par e negativo.')
-# elif numero % 2 != 0 and numero > 0:
-#     print('Impar e positivo')
-# elif numero % 2 != 0 and numero < 0:
-#     print('impar e negativo')
+if numero % 2 == 0 and numero>0:
+    print('É par e positivo')
+elif numero % 2 == 0 and numero < 0:
+    print('É par e negativo.')
+elif numero % 2 != 0 and numero > 0:
+    print('Impar e positivo')
+elif numero % 2 != 0 and numero < 0:
+    print('impar e negativo')
 
 # Atividade 10:
 # Verificar Classificação de IMC:
 # Crie um programa que calcule o Índice de Massa Corporal (IMC)
 # e use if para classificar o resultado em 'Abaixo do peso,Peso normal,'Sobrepeso' e Obesidade.
 
-# peso =  float(input('Digite seu peso:'))
-# altura = float(input('Digite sua altura:'))
+peso =  float(input('Digite seu peso:'))
+altura = float(input('Digite sua altura:'))
 
-# imc = peso / (altura**2)
+imc = peso / (altura**2)
 
-# if imc < 19.4:
-#     print('Abaixo do peso')
-# elif imc >= 18.5 and imc <= 24.9:
-#     print('Peso normal')
-# elif imc >= 25 and imc < 30:
-#     print('peso normal')
-# elif imc >= 25 and imc <= 30:
-#     print('Sobrepeso')
-# else:
-#     print('Obesidade')
+if imc < 19.4:
+    print('Abaixo do peso')
+elif imc >= 18.5 and imc <= 24.9:
+    print('Peso normal')
+elif imc >= 25 and imc < 30:
+    print('peso normal')
+elif imc >= 25 and imc <= 30:
+    print('Sobrepeso')
+else:
+    print('Obesidade')
 
 # Atividade 11:
 # Sistema de Login Simples:
@@ -95,18 +95,14 @@
 # e uma senha e use if para verificar se são iguais a "admin" e "1234"
 # ,respectivamente.
 
-# usuario = input('Digite o nome de usuario:')
-# senha = input('Digite a senha:')
+usuario = input('Digite o nome de usuario:')
+senha = input('Digite a senha:')
 
-# if usuario =='admin' and senha == "1234":
-#     print('Usuario autenticado.')
-# else:
-#     print('Credenciais incorretas.')
+if usuario =='admin' and senha == "1234":
+    print('Usuario autenticado.')
+else:
+    print('Credenciais incorretas.')
 
-# Atividade 11:
-# Sistema de Login Simples:
-# Desenvolva um programa que peça ao usuário um nome de usuário
-# e uma senha e use if para verificar se são iguais a "admin" e "1234"
 
 
 
